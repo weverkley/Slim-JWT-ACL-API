fd71dbd9c4fba4783a467c351261319731153a46
Wever Kley <wever.kley@siit.com.br>
2018-11-24 09:07:08 -0200

    Variable for path

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+define('PUBLIC_FOLDER', realpath(dirname(__FILE__)));
+define('ROOT_DIR', explode('/public', PUBLIC_FOLDER)[0]);
+define('DS', DIRECTORY_SEPARATOR);
+
 // Allow from any origin
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
@@ -8,37 +12,44 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
 
 // Angular request options
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && (
-			$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST' ||
-			$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'DELETE' ||
-			$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'PUT' )
-	) {
-		header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS'); // http://stackoverflow.com/a/7605119/578667
-	}
-	
-	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
-		header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, App-token');
-	}
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && (
+        $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST' ||
+        $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'DELETE' ||
+        $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'PUT')
+    ) {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS'); // http://stackoverflow.com/a/7605119/578667
+    }
 
-	exit;
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, App-token');
+    }
+
+    exit;
 }
 
 // Composer autoload dependencies
-require __DIR__ . '/../vendor/autoload.php';
+$autoload = ROOT_DIR . DS . 'vendor' . DS . 'autoload.php';
+if (file_exists($autoload)) {
+    require $autoload;
+} else {
+    echo 'File: ' . $autoload . ' doesnt exist';
+    echo 'You must install composer first: composer install';
+    exit;
+}
 
 // Load .env file
-$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv = new Dotenv\Dotenv(ROOT_DIR);
 $dotenv->load();
 
-if ((bool)getenv('API_DEBUG')) {
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+if ((bool) getenv('API_DEBUG')) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 }
 
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $url = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
     if (is_file($file)) {
         return false;
@@ -48,23 +59,23 @@ if (PHP_SAPI == 'cli-server') {
 session_start();
 
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require ROOT_DIR . DS . 'src' . DS . 'settings.php';
 $app = new \Slim\App($settings);
 
 // Set up app
-require __DIR__ . '/../src/dependency.php';
+require ROOT_DIR . DS . 'src' . DS . 'dependency.php';
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require ROOT_DIR . DS . 'src' . DS . 'middleware.php';
 
 // Set up errors
-require __DIR__ . '/../src/errors.php';
+require ROOT_DIR . DS . 'src' . DS . 'errors.php';
 
 // Set up DI
-require __DIR__ . '/../App/DI.php';
+require ROOT_DIR . DS . 'App' . DS . 'DI.php';
 
 // Register routes
-require __DIR__ . '/../App/routes.php';
+require ROOT_DIR . DS . 'App' . DS . 'routes.php';
 
 // Run app
 $app->run();
